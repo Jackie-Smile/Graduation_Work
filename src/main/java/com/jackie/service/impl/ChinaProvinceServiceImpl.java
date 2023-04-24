058546e7ee58e617fe990471ac786963ade39974
Jackie-Smile <350661349@qq.com>
2023-04-24 11:10:53 +0800

    changed the api of china_new.jsp and province_today.jsp

--- src/main/java/com/jackie/service/impl/ChinaProvinceServiceImpl.java
+++ src/main/java/com/jackie/service/impl/ChinaProvinceServiceImpl.java
@@ -22,49 +22,55 @@ public class ChinaProvinceServiceImpl implements ChinaProvinceService
     @Override
     public List<ChinaProvince> getnewconfirm()
     {
-        List<ChinaProvince> chinaProvinceList=new ArrayList<ChinaProvince>();
-        StringBuilder json = new StringBuilder();
-        try
         {
-            URL urlObject = new URL("https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5");
-            URLConnection uc = urlObject.openConnection();
-            BufferedReader bin = new BufferedReader(new InputStreamReader(uc.getInputStream(), "utf-8"));
-            String inputLine = null;
-            while ((inputLine = bin.readLine()) != null) {
-                json.append(inputLine);
-            }
-            bin.close();
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(String.valueOf(json));
-        String data = "data";
-        if (element.isJsonObject()) {
-            JsonObject object = element.getAsJsonObject();
-            data = object.get("data").getAsString();
-        }
-        element = parser.parse(data);
-        if (element.isJsonObject())
-        {
-            JsonObject object = element.getAsJsonObject();
-            JsonArray areaTree = object.get("areaTree").getAsJsonArray();
-            JsonObject china=areaTree.get(0).getAsJsonObject();
-            JsonArray children=china.get("children").getAsJsonArray();
-            for (int i = 0; i < children.size(); i++)
+
+            List<ChinaProvince> chinaProvinceList=new ArrayList<ChinaProvince>();
+            StringBuilder json = new StringBuilder();
+            try
             {
-                ChinaProvince chinaProvince=new ChinaProvince();
-                JsonObject current=children.get(i).getAsJsonObject();
-                String name=current.get("name").getAsString();
-                JsonObject today=current.get("today").getAsJsonObject();
-                int confirm=today.get("confirm").getAsInt();
-                chinaProvince.setName(name);
-                chinaProvince.setValue(confirm);
-                chinaProvinceList.add(chinaProvince);
+                URL urlObject = new URL("https://c.m.163.com/ug/api/wuhan/app/data/list-total");
+                URLConnection uc = urlObject.openConnection();
+                uc.setRequestProperty("User-Agent", "Mozilla/4.76");
+                BufferedReader bin = new BufferedReader(new InputStreamReader(uc.getInputStream(), "utf-8"));
+                String inputLine = null;
+                while ((inputLine = bin.readLine()) != null) {
+                    json.append(inputLine);
+                }
+                bin.close();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
             }
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(String.valueOf(json));
+//        System.out.println(element.getAsJsonObject().get("data"));
+//        String data = "data";
+//        if (element.isJsonObject()) {
+//            JsonObject object = element.getAsJsonObject();
+//            data = object.get("data").getAsString();
+//        }
+            element = element.getAsJsonObject().get("data");
+            System.out.println(element);
+            if (element.isJsonObject())
+            {
+                JsonObject object = element.getAsJsonObject();
+                JsonArray areaTree = object.get("areaTree").getAsJsonArray();
+                JsonObject china=areaTree.get(2).getAsJsonObject();
+                JsonArray children=china.get("children").getAsJsonArray();
+                for (int i = 0; i < children.size(); i++)
+                {
+                    ChinaProvince chinaProvince=new ChinaProvince();
+                    JsonObject current=children.get(i).getAsJsonObject();
+                    String name=current.get("name").getAsString();
+                    JsonObject today=current.get("today").getAsJsonObject();
+                    int confirm=today.get("confirm").getAsInt();
+                    chinaProvince.setName(name);
+                    chinaProvince.setValue(confirm);
+                    chinaProvinceList.add(chinaProvince);
+                }
+            }
+            return chinaProvinceList;
         }
-        return chinaProvinceList;
     }
 
     @Override
@@ -74,8 +80,9 @@ public class ChinaProvinceServiceImpl implements ChinaProvinceService
         StringBuilder json = new StringBuilder();
         try
         {
-            URL urlObject = new URL("http://111.231.75.86:8000/api/provinces/CHN/");
+            URL urlObject = new URL("https://c.m.163.com/ug/api/wuhan/app/data/list-total");
             URLConnection uc = urlObject.openConnection();
+            uc.setRequestProperty("User-Agent", "Mozilla/4.76");
             BufferedReader bin = new BufferedReader(new InputStreamReader(uc.getInputStream(), "utf-8"));
             String inputLine = null;
             while ((inputLine = bin.readLine()) != null) {
@@ -88,21 +95,31 @@ public class ChinaProvinceServiceImpl implements ChinaProvinceService
         }
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(String.valueOf(json));
+//        System.out.println(element.getAsJsonObject().get("data"));
+//        String data = "data";
+//        if (element.isJsonObject()) {
+//            JsonObject object = element.getAsJsonObject();
+//            data = object.get("data").getAsString();
+//        }
+        element = element.getAsJsonObject().get("data");
         System.out.println(element);
-        element = element.getAsJsonArray();
-        System.out.println(element);
-        for(int i=0;i<element.getAsJsonArray().size();i++){
-            ChinaProvince chinaProvince=new ChinaProvince();
-            JsonObject current=element.getAsJsonArray().get(i).getAsJsonObject();
-            System.out.println(current);
-            String name=current.get("provinceName").getAsString();
-            System.out.println(name);
-            int confirm=current.get("confirmedCount").getAsInt();
-            System.out.println(confirm);
-            chinaProvince.setName(name);
-            chinaProvince.setValue(confirm);
-            chinaProvinceList.add(chinaProvince);
-            System.out.println(chinaProvince);
+        if (element.isJsonObject())
+        {
+            JsonObject object = element.getAsJsonObject();
+            JsonArray areaTree = object.get("areaTree").getAsJsonArray();
+            JsonObject china=areaTree.get(2).getAsJsonObject();
+            JsonArray children=china.get("children").getAsJsonArray();
+            for (int i = 0; i < children.size(); i++)
+            {
+                ChinaProvince chinaProvince=new ChinaProvince();
+                JsonObject current=children.get(i).getAsJsonObject();
+                String name=current.get("name").getAsString();
+                JsonObject today=current.get("total").getAsJsonObject();
+                int confirm=today.get("confirm").getAsInt();
+                chinaProvince.setName(name);
+                chinaProvince.setValue(confirm);
+                chinaProvinceList.add(chinaProvince);
+            }
         }
         Collections.sort(chinaProvinceList);
         return chinaProvinceList;
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -12,12 +12,14 @@ import com.jackie.domain.News;
 
 public class test {
     public static void main(String[] args) {
+
         List<ChinaProvince> chinaProvinceList=new ArrayList<ChinaProvince>();
         StringBuilder json = new StringBuilder();
         try
         {
-            URL urlObject = new URL("http://111.231.75.86:8000/api/provinces/CHN/");
+            URL urlObject = new URL("https://c.m.163.com/ug/api/wuhan/app/data/list-total");
             URLConnection uc = urlObject.openConnection();
+            uc.setRequestProperty("User-Agent", "Mozilla/4.76");
             BufferedReader bin = new BufferedReader(new InputStreamReader(uc.getInputStream(), "utf-8"));
             String inputLine = null;
             while ((inputLine = bin.readLine()) != null) {
@@ -30,48 +32,32 @@ public class test {
         }
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(String.valueOf(json));
-        System.out.println(element);
-        element = element.getAsJsonArray();
-        System.out.println(element);
-        for(int i=0;i<element.getAsJsonArray().size();i++){
-            ChinaProvince chinaProvince=new ChinaProvince();
-            JsonObject current=element.getAsJsonArray().get(i).getAsJsonObject();
-            System.out.println(current);
-            String name=current.get("provinceName").getAsString();
-            System.out.println(name);
-            int confirm=current.get("currentConfirmedCount").getAsInt();
-            System.out.println(confirm);
-            chinaProvince.setName(name);
-            chinaProvince.setValue(confirm);
-            chinaProvinceList.add(chinaProvince);
-            System.out.println(chinaProvince);
-        }
+//        System.out.println(element.getAsJsonObject().get("data"));
 //        String data = "data";
 //        if (element.isJsonObject()) {
 //            JsonObject object = element.getAsJsonObject();
 //            data = object.get("data").getAsString();
 //        }
-//        element = parser.parse(data);
-//        if (element.isJsonObject())
-//        {
-//            JsonObject object = element.getAsJsonObject();
-//            JsonArray areaTree = object.get("areaTree").getAsJsonArray();
-//            JsonObject china=areaTree.get(0).getAsJsonObject();
-//            JsonArray children=china.get("children").getAsJsonArray();
-//            for (int i = 0; i < children.size(); i++)
-//            {
-//                ChinaProvince chinaProvince=new ChinaProvince();
-//                JsonObject current=children.get(i).getAsJsonObject();
-//                System.out.println(current);
-//                String name=current.get("provinceName").getAsString();
-//                JsonObject total=current.get("total").getAsJsonObject();
-//                int confirm=current.get("currentConfirmedCount").getAsInt();
-//                chinaProvince.setName(name);
-//                chinaProvince.setValue(confirm);
-//                chinaProvinceList.add(chinaProvince);
-//            }
-//        }
-        Collections.sort(chinaProvinceList);
+        element = element.getAsJsonObject().get("data");
+        System.out.println(element);
+        if (element.isJsonObject())
+        {
+            JsonObject object = element.getAsJsonObject();
+            JsonArray areaTree = object.get("areaTree").getAsJsonArray();
+            JsonObject china=areaTree.get(2).getAsJsonObject();
+            JsonArray children=china.get("children").getAsJsonArray();
+            for (int i = 0; i < children.size(); i++)
+            {
+                ChinaProvince chinaProvince=new ChinaProvince();
+                JsonObject current=children.get(i).getAsJsonObject();
+                String name=current.get("name").getAsString();
+                JsonObject today=current.get("today").getAsJsonObject();
+                int confirm=today.get("confirm").getAsInt();
+                chinaProvince.setName(name);
+                chinaProvince.setValue(confirm);
+                chinaProvinceList.add(chinaProvince);
+            }
+        }
 //        return chinaProvinceList;
     }
 }
